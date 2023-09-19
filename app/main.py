ee25830f5057267b9c3c48aa2b336cb94b3abfa1
Rhayar Mascarello <921355@lojasrenner.com.br>
2023-09-19 10:59:22 -0300

    commit changes

--- app/main.py
+++ app/main.py
@@ -16,38 +16,43 @@ def custom_css(font_size):
 
 custom_css("18px")
 
-st.title("DP203 Test")
+st.title("DP203 Exam")
 
 # Donation Note at the top
 st.markdown("If you found this app helpful, consider making a donation to support development. Thank you! 🙏")
 st.markdown("Bitcoin (BTC) Address: 🔗 `bc1qkru3pwy9jhn2gsmet245ssd9xx5mlzcvpgczpy`")
 st.markdown("Polygon Address: 🔗 `0x5eBDB6031BeCa2A9a538A00477dC8049244e86fe`")
 
-# Initialize score and timer
-correct_answers = 0
-incorrect_answers = 0
-start_time = None
-end_time = None
+# Initialize or reset session state variables
+if 'correct_answers' not in st.session_state:
+    st.session_state.correct_answers = 0
+if 'incorrect_answers' not in st.session_state:
+    st.session_state.incorrect_answers = 0
+if 'start_time' not in st.session_state:
+    st.session_state.start_time = None
+if 'end_time' not in st.session_state:
+    st.session_state.end_time = None
 
 # Timer Buttons & Display at the top
 if st.button('Start'):
-    start_time = time.time()
+    st.session_state.start_time = time.time()
     
 if st.button('Stop'):
-    if start_time:
-        end_time = time.time()
+    st.session_state.end_time = time.time()
 
 if st.button('Reset'):
-    start_time = None
-    end_time = None
+    st.session_state.start_time = None
+    st.session_state.end_time = None
+    st.session_state.correct_answers = 0
+    st.session_state.incorrect_answers = 0
 
-if start_time:
-    elapsed_time = time.time() - start_time
+if st.session_state.start_time:
+    elapsed_time = time.time() - st.session_state.start_time
 else:
     elapsed_time = 0
 
-if end_time:
-    elapsed_time = end_time - start_time
+if st.session_state.end_time:
+    elapsed_time = st.session_state.end_time - st.session_state.start_time
 
 st.markdown(f"**Timer**: {int(elapsed_time // 60)} minutes and {int(elapsed_time % 60)} seconds")
 
@@ -59,16 +64,16 @@ for idx, q in enumerate(questions, 1):
 
     if st.button(f"Check Answer for Question {idx}"):
         if answer == q['answer']:
-            correct_answers += 1
+            st.session_state.correct_answers += 1
             st.success(f"Correct! The answer is {q['answer']}.")
             st.info(f"Explanation: {q['explanation']}")
         else:
-            incorrect_answers += 1
+            st.session_state.incorrect_answers += 1
             st.error(f"Wrong answer. The correct answer is {q['answer']} and the explanation is: \n\n {q['explanation']}.")
 
 # Display score and timer at the bottom
-st.markdown(f"**Correct Answers**: {correct_answers}")
-st.markdown(f"**Incorrect Answers**: {incorrect_answers}")
+st.markdown(f"**Correct Answers**: {st.session_state.correct_answers}")
+st.markdown(f"**Incorrect Answers**: {st.session_state.incorrect_answers}")
 st.markdown(f"**Timer**: {int(elapsed_time // 60)} minutes and {int(elapsed_time % 60)} seconds")
 
 # Donation Note at the bottom
